35d87c32395ecb7dcc61d7586701a7b4fbe4835d
Ben Doan <ben@bendoan.me>
2017-01-26 16:25:25 -0600

    added utility for querying database outside of app

--- code_court/courthouse/util.py
+++
@@ -1,8 +0,0 @@
-import json
-
-from collections import OrderedDict
-from os import path
-import sys
-import logging
-
-SCRIPT_PATH = path.dirname(path.realpath(__file__))
--- code_court/courthouse/views/main.py
+++ code_court/courthouse/views/main.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template, send_from_directory
-from util import *
 
 main = Blueprint('main', __name__,
                  template_folder='templates')
