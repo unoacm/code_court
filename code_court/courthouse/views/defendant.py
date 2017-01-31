26c0d75ee3f2e624f42733fa9397c79013eab6d1
snobbs <snobbs@unomaha.edu>
2017-01-30 18:00:43 -0600

    Made initial frontend blueprints (view and template) (#51)

---
+++ code_court/courthouse/views/defendant.py
@@ -0,0 +1,18 @@
+import json
+import re
+import sqlalchemy
+
+from flask import (
+    Blueprint,
+    render_template,
+)
+
+defendant = Blueprint('defendant', __name__,
+                  template_folder='templates')
+
+@defendant.route("/", methods=["GET"])
+def index():
+    """
+    The index page for the defendant frontend
+    """
+    return render_template("defendant_index.html")
