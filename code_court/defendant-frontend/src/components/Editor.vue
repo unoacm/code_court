2d71c6e3f52bea015cbc65b22f65302aa4e7a2d3
Ben Doan <ben@bendoan.me>
2017-03-04 14:38:32 -0600

    added defendant-frontend build config

---
+++ code_court/defendant-frontend/src/components/Editor.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+    <textarea></textarea>
+  </div>
+</template>
+
+<script>
+import brace from 'brace'
+export default {
+  data () {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
