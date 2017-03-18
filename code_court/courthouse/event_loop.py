f450e51c7316a6de916a0c5a2be2f4595658ae2c
Josiah <spacetagman@gmail.com>
2017-03-18 16:23:20 -0500

    Give overdue runs another chance at being exec'd

--- code_court/courthouse/event_loop.py
+++ code_court/courthouse/event_loop.py
@@ -1,8 +1,31 @@
+import datetime
 from time import sleep
 
-try:
-    while True:
-        print("We're looping!")
-        sleep(30)
-except KeyboardInterrupt:
-    print("Event loop shutting down")
+import model
+from model import db
+
+EXECUTOR_TIMEOUT_MINS = 5
+
+def reset_overdue_runs():
+    runs = model.Run.query.filter(model.Run.finished_execing_time == None)\
+               .filter(model.Run.started_execing_time != None)\
+               .filter((datetime.datetime.utcnow() -
+                        datetime.timedelta(minutes=EXECUTOR_TIMEOUT_MINS)) >
+                        model.Run.started_execing_time).all()
+
+    for run in runs:
+        run.started_execing_time = None
+
+    model.db.session.commit()
+
+def event_loop():
+    try:
+        while True:
+            reset_overdue_runs()
+            sleep(30)
+
+    except KeyboardInterrupt:
+        print("Event loop shutting down")
+
+if __name__ == "__main__":
+    event_loop()
