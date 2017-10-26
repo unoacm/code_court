86e593ea50d8937d71d465a327896a6b08d28329
Ben Doan <ben@bendoan.me>
2017-10-26 11:55:34 -0500

    Revert "added js tests"
    This reverts commit d95ee1114f17d4eecbad7ccade8df28d6168d1ed.

--- code_court/defendant-frontend/test/unit/karma.conf.js
+++ code_court/defendant-frontend/test/unit/karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function (config) {
     browsers: ['PhantomJS'],
     frameworks: ['mocha', 'sinon-chai'],
     reporters: ['spec', 'coverage'],
-    files: ['../../node_modules/babel-polyfill/dist/polyfill.js','./index.js'],
+    files: ['./index.js'],
     preprocessors: {
       './index.js': ['webpack', 'sourcemap']
     },
--- code_court/defendant-frontend/test/unit/specs/Notification.spec.js
+++ code_court/defendant-frontend/test/unit/specs/Notification.spec.js
@@ -1,14 +1,11 @@
 import Vue from 'vue'
-import Notification from '@/components/Notification'
+import Hello from '@/components/Hello'
 
-describe('Notification.vue', () => {
+describe('Hello.vue', () => {
   it('should render correct contents', () => {
-    const propsData = {
-      message: 'hello'
-    }
-
-    const Constructor = Vue.extend(Notification)
-    const vm = new Constructor({ propsData }).$mount()
-    expect(vm.$el.querySelector('.notification').textContent.trim()).to.equal('hello')
+    const Constructor = Vue.extend(Hello)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('.hello h1').textContent)
+      .to.equal('Welcome to Your Vue.js App')
   })
 })
