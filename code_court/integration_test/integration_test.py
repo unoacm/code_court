1720c71aa92dc38bd87bacb53c29c042ceba1443
Ben Doan <ben@bendoan.me>
2017-10-27 20:12:30 -0500

    wait for postgres to start in integration test

--- code_court/integration_test/integration_test.py
+++ code_court/integration_test/integration_test.py
@@ -4,10 +4,12 @@ import logging
 import os
 import signal
 import subprocess
+import sys
 import time
 
 from selenium import webdriver
 from docker.errors import ImageNotFound, ContainerError, APIError
+from docker.api import exec_api
 import docker
 
 logging.basicConfig(level=logging.INFO)
@@ -20,24 +22,32 @@ POSTGRES_ENVIRON = {
     "POSTGRES_DB": "codecourt",
 }
 
+client = docker.from_env()
+
 def main():
     postgres_container = None
     courthouse_pid = None
     try:
         logging.info("Starting postgres")
-        client = docker.from_env()
         ports = {"5432/tcp": POSTGRES_PORT}
         postgres_container = client.containers.run("postgres",
                                                    detach=True,
                                                    environment=POSTGRES_ENVIRON,
                                                    ports=ports)
 
-        #TODO: wait for pg to start
-        time.sleep(10)
+        # wait for postgres to start
+        iterations = 0
+        while not is_postgres_up(postgres_container):
+            logging.info("Waiting for postgres to come up")
+            time.sleep(1)
+            iterations += 1
+
+            if iterations == 30:
+                logging.fatal("Postgres failed to start")
+                sys.exit(1)
+        time.sleep(5)
 
         courthouse_pid = start_courthouse()
-
-        time.sleep(5)
 
         run_tests()
     except ImageNotFound:
@@ -71,9 +81,22 @@ def start_courthouse():
     return courthouse_master_pid
 
 
+def is_postgres_up(container):
+    cmd = 'psql -U postgres -c "select 1" -d postgres'
+    exec_job = client.api.exec_create(container.id, cmd)
+
+    client.api.exec_start(exec_job['Id'])
+    status = client.api.exec_inspect(exec_job['Id'])
+
+    return status['ExitCode'] == 0
+
+
 def run_tests():
     driver = webdriver.Chrome()
     driver.get("http://localhost:9191/admin")
+
+    driver.find_element_by_css_selector
+
     driver.implicitly_wait(10)
 
 if __name__ == '__main__':
