7a7865aa48732e34822f28f743a8b0b4ff61d5a0
pgayee694 <36524925+pgayee694@users.noreply.github.com>
2018-03-23 11:37:56 -0500

    Update dev-server.js

--- code_court/defendant-frontend/build/dev-server.js
+++ code_court/defendant-frontend/build/dev-server.js
@@ -63,7 +63,7 @@ app.use(hotMiddleware)
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
-var uri = 'localhost:' + port
+var uri = 'http://localhost:' + port
 
 devMiddleware.waitUntilValid(function () {
   console.log('> Listening at ' + uri + '\n')
