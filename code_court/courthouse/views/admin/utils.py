cc05e58c3cac35fbdc8bc674c295347733e51be1
Ben Doan <ben@bendoan.me>
2019-03-10 16:17:35 -0500

    add page for invalidating uwsgi caches
    Fixes #290

--- code_court/courthouse/views/admin/utils.py
+++ code_court/courthouse/views/admin/utils.py
@@ -25,3 +25,13 @@ def random_contestants():
 
     shuffled_contestants = random.sample(contestants, k=num)
     return render_template("utils/random-contestants.html", contestants=shuffled_contestants)
+
+@utils.route("/invalidate-caches/", methods=["GET"])
+@util.login_required("operator")
+def invalidate_caches():
+    """
+    Invalidates all uwsgi caches
+    """
+    util.invalidate_cache(util.RUN_CACHE_NAME)
+    util.invalidate_cache(util.SCORE_CACHE_NAME)
+    return render_template("message.html", message="Invalidated cache")
